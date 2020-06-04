f5682ef50dfb2e93e06b9500edf42cd61884e339
Vladimir <vladkodoktorov@gmail.com>
2020-06-04 17:37:42 +0100

    updated packages + rendered umbraco content through mvc

--- UmbracoWithMvc/Controllers/HomeController.cs
+++ UmbracoWithMvc/Controllers/HomeController.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Umbraco.Web;
 
 namespace UmbracoWithMvc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UmbracoContext umbracoContext;
+
+        public HomeController()
+        {
+            umbracoContext = UmbracoContext.Current; 
+        }
         public ActionResult Index()
         {
+           // var user = umbracoContext.Security.CurrentUser.Email;
+           var rootNode = umbracoContext.ContentCache.GetById(1073);
+
+           var siteTitle = rootNode.GetPropertyValue<string>("siteTitle");
+
+            ViewBag.SiteTitle = siteTitle;
+           // ViewBag.User = user;
+
             return View();
         }
 
