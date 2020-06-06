352d932fc669e371648b3c84b074ab327f348683
Vladimir <vladkodoktorov@gmail.com>
2020-06-06 11:30:51 +0100

    added new template, controller form.

---
+++ UmbracoWithMvc/Models/ContactUsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UmbracoWithMvc.Models
+{
+    public class ContactUsModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Maximum 300 Characters")]
+        [StringLength(300)]
+        public string Message { get; set; }
+    }
+}
